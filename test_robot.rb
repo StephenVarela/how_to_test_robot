28bc5c97ec5a4b7ca3159487bf972b99cd65a1c8
StephenVarela <varelast92@gmail.com>
2018-10-15 13:52:46 -0400

    tested up to returning max value from todo list, added setup method to DRY code

--- test_robot.rb
+++ test_robot.rb
@@ -4,58 +4,73 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
-  def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
-    # arrange
+  def setup
+    @my_robot = Robot.new
+    @my_robot.needs_repairs = false
+    @my_robot.foreign_model = false
+    @my_robot.vintage_model = false
+  end
 
+  def test_that_foreign_robot_needing_repairs_sent_to_station_1
+    # arrange
+    @my_robot.needs_repairs = true
+    @my_robot.foreign_model = true
     # act
+    return_value = @my_robot.station
 
     # assert
+    assert_equal(1, return_value)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
+    @my_robot.needs_repairs = true
+    @my_robot.vintage_model = true
 
     # act
+    return_value = @my_robot.station
 
     # assert
+    assert_equal(2, return_value)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    @my_robot.needs_repairs = true
     # act
+    return_value = @my_robot.station
 
     # assert
+    assert_equal(3, return_value)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
     # act
+    return_value = @my_robot.station
 
     # assert
+    assert_equal(4, return_value)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
-
     # act
+    return_value = @my_robot.prioritize_tasks
 
     # assert
+    assert_equal(-1, return_value)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
-
+    robots_todos = [5,3,9,2]
+    @my_robot.todos = robots_todos
     # act
+    return_value = @my_robot.prioritize_tasks
 
     # assert
+    assert_equal(9, return_value)
   end
 
   def test_workday_on_day_off_returns_false
